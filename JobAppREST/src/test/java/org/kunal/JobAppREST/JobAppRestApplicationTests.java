0d099ba1746df7294948bab125697f0cbb427662
Kunal Saha <kunalsaha1107@gmail.com>
2025-02-18 22:34:01 +0530

    init

---
+++ JobAppREST/src/test/java/org/kunal/JobAppREST/JobAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kunal.JobAppREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
